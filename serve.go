b3b6ac2fa461d553243c9f848fdeb6995d51694a
Jack Schefer <jack.schefer9@gmail.com>
2016-09-24 19:40:10 -0400

    more work

--- serve.go
+++ serve.go
@@ -30,44 +30,14 @@ func index_handler(w http.ResponseWriter, r *http.Request) {
 }
 //
 func about_handler(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintf(w, header() + "<h1>You are on page " + r.URL.Path + "</h1>")
+  fmt.Fprintf(w, header("About") + "<h1>You are on page " + r.URL.Path + "</h1>")
 }
 func play_handler(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintf(w, header() "<h1>You are on page " + r.URL.Path + "</h1>")
+  fmt.Fprintf(w, header("Play") + "<h1>You are on page " + r.URL.Path + "</h1>")
 }
 //
-func header() string {
-      return "<div class=\"link-wrapper\">
-         <div class=\"container\">
-            <div class=\"row\">
-               <div class=\"col-sm-6\"></div>
-               <div class=\"col-sm-2\">
-                  <a href=\"index.html\">
-                     Home
-                  </a>
-               </div>
-               <div class=\"col-sm-2\">
-                  <a href=\"about.html\">
-                     About
-                  </a>
-               </div>
-               <div class=\"col-sm-2\">
-                  <a href=\"play.html\">
-                     Play
-                  </a>
-               </div>
-            </div>
-         </div>
-      <div class=\"header-wrapper\">
-         <div class=\"container\">
-            <div class=\"row\">
-               <div class=\"col-xs-12\">
-                  <h1>Welcome to {{.Title}}</h1>
-                  <h3>A QuizBowl Site for the Future</h3>
-               </div>
-            </div>
-         </div>
-      </div>"
+func header(title string) string {
+   return "<div class=\"link-wrapper\"> <div class=\"container\"> <div class=\"row\"> <div class=\"col-sm-6\"></div> <div class=\"col-sm-2\"> <a href=\"index.html\"> Home </a> </div> <div class=\"col-sm-2\"> <a href=\"about.html\"> About </a> </div> <div class=\"col-sm-2\"> <a href=\"play.html\"> Play </a> </div> </div> </div> <div class=\"header-wrapper\"> <div class=\"container\"> <div class=\"row\"> <div class=\"col-xs-12\"> <h1>Welcome to " + title + "</h1> <h3>A QuizBowl Site for the Future</h3> </div> </div> </div> </div>"
 }
 //
 //
